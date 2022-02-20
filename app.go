bf1c6221a3b297c6ce4d7baf2bfa3a91445d9f45
kecheon <kecheon@gmail.com>
2022-02-20 16:57:53 +0900

    workin on lesson9

--- app.go
+++ app.go
@@ -9,5 +9,6 @@ import (
 func main() {
 	di.Greet(os.Stdout, "Foo")
 	// log.Fatal(http.ListenAndServe(":8000", http.HandlerFunc(di.MyGreetHandler)))
-	mocking.Countdown(os.Stdout)
+	sleeper := &mocking.DefaultSleeper{}
+	mocking.Countdown(os.Stdout, sleeper)
 }
--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,14 +3,34 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWord+"\n")
+}
+
+type Sleeper interface {
+	Sleep()
+}
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
 }
