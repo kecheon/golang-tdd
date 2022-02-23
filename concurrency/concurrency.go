1bb7627b48f3382235ad83cd1d6aa8db6b82e0fe
kecheon <kecheon@gmail.com>
2022-02-23 14:13:31 +0900

    lesson11 concurrency

---
+++ concurrency/concurrency.go
@@ -0,0 +1,44 @@
+package concurrency
+
+import (
+	"testing"
+	"time"
+)
+
+type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+
+	for _, url := range urls {
+		go func(item string) {
+			resultChannel <- result{item, wc(item)}
+		}(url)
+	}
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+	return results
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
