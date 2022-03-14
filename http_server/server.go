3b9f953d62fc97e349e1f6acfa3037e9bfea5384
kecheon <kecheon@gmail.com>
2022-03-14 10:37:00 +0900

    embdding ServeHTTP into http.Handler

--- http_server/server.go
+++ http_server/server.go
@@ -12,22 +12,19 @@ type PlayerStore interface {
 }
 
 type PlayerServer struct {
-	Store  PlayerStore
-	router *http.ServeMux
+	Store PlayerStore
+	http.Handler
 }
 
 func NewPlayerServer(store PlayerStore) *PlayerServer {
-	p := &PlayerServer{
-		store,
-		http.NewServeMux(),
-	}
-	p.router.Handle("/league", http.HandlerFunc(p.leagueHandler))
-	p.router.Handle("/players/", http.HandlerFunc(p.playersHandler))
-	return p
-}
+	p := new(PlayerServer)
+	p.Store = store
+	router := http.NewServeMux()
 
-func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	p.router.ServeHTTP(w, r)
+	router.Handle("/league", http.HandlerFunc(p.leagueHandler))
+	router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+	p.Handler = router
+	return p
 }
 
 func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
